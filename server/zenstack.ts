9f4c7dcdc11496046ed3f3a49ee744adc9b512a4
stevie <yzm791@qq.com>
2025-09-03 15:12:00 +0800

    -

--- server/zenstack.ts
+++ server/zenstack.ts
@@ -1,9 +1,10 @@
 import { ZenStackClient } from '@zenstackhq/runtime';
 import { schema } from '../zenstack/schema';
+import { SqliteDialect } from 'kysely';
 import SQLite from 'better-sqlite3';
 
 
 export default new ZenStackClient(schema, {
-    dialectConfig: {database: new SQLite('./zenstack/dev.db')},
+    dialect: new SqliteDialect({ database: new SQLite('./zenstack/dev.db') }),
     log: ['query', 'error']
 });
