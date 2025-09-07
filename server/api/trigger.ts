11b9252beb5ef7109272559e1ccadfcfef5b079a
stevie <yzm791@qq.com>
2025-09-07 10:01:47 +0800

    chore: update files
    Update project files with minor changes.

--- server/api/trigger.ts
+++ server/api/trigger.ts
@@ -1,10 +1,7 @@
 import { helloWorldTask } from "../../trigger/example";
-import { runs } from "@trigger.dev/sdk";
 
 export default defineEventHandler(async (event) => {
     const handle = await helloWorldTask.trigger({ message: "Hello world!" });
 
-    const run = await runs.retrieve(handle);
-
-    return {handle, run}
+    return {handle}
 })
