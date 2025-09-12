a078be973e0b4a4caf2a72930766e7835bc8ee2a
stevie <yzm791@qq.com>
2025-09-12 11:42:42 +0800

    chore: 移除Trigger.dev相关依赖和代码
    删除Trigger.dev的SDK依赖项及相关示例代码文件，清理不再使用的功能。

--- server/api/trigger.ts
+++
@@ -1,7 +0,0 @@
-// import { helloWorldTask } from "../../trigger/example";
-
-// export default defineEventHandler(async (event) => {
-//     const handle = await helloWorldTask.trigger({ message: "Hello world!" });
-
-//     return {handle}
-// })
