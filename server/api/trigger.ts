30464cb981e4f6334a2ad6dac988935a63a54360
stevie <yzm791@qq.com>
2025-09-14 11:52:40 +0800

    fix(trigger): 注释掉Vercel环境下的任务触发代码
    移除在非Vercel环境下触发helloWorldTask的逻辑，避免构建问题。

--- server/api/trigger.ts
+++ server/api/trigger.ts
@@ -2,9 +2,9 @@ import { helloWorldTask } from "../../trigger/example";
 // 注 vercel 构建后无法显示
 export default defineEventHandler(async (event) => {
     let handle = null;
-    if(!process.env.VERCEL){
-        handle = await helloWorldTask.trigger({ message: "Hello world!" });
-    }
+    // if(!process.env.VERCEL){
+    //     handle = await helloWorldTask.trigger({ message: "Hello world!" });
+    // }
 
     return {handle}
 })
